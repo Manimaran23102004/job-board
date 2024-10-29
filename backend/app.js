ac9ed662f08bc86def1a0f25c9541de4aca67965
Manimaran23102004 <manimaranganeshkumar@gmail.com>
2024-10-29 10:14:37 +0530

    new commite

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require("body-parser")
 const cors = require('cors');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
 dotenv.config();
 
 
@@ -17,7 +20,14 @@ mongoose.connect(process.env.DATABASE,{
 }).then(()=>console,log("DB Connected"))
 .catch((err)=>console.log(err)); 
 
-
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit:"5mb"}))
+app.use(bodyParser.urlencoded({
+    limit:"5mb",
+    extended:true
+}));
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 6000
 
